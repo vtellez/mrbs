c98f078969b0b16cf6a14156b5983388f72798c0
Víctor Téllez <tellez.victor@gmail.com>
2014-08-07 00:18:21 +0200

    fixed

--- lib/parser.php
+++ lib/parser.php
@@ -66,6 +66,12 @@ function parseFile ($file, $bdhost, $bduser, $bdpass, $bdname, $pod_user_id) {
       // | custom_html      | text        | YES  |     | NULL    |                |
       // +------------------+-------------+------+-----+---------+----------------+
 
+      // +-----+----------+---------+---------------------------+---------------------------+-------------+----------+---------------------------------+-------------+
+      // | id  | disabled | area_id | room_name                 | sort_key                  | description | capacity | room_admin_email                | custom_html |
+      // +-----+----------+---------+---------------------------+---------------------------+-------------+----------+---------------------------------+-------------+
+      // |  27 |        0 |       8 | Sala Juntas M             | Sala Juntas M             |             |        0 | resaumed@listas.us.es           |             |
+      // +-----+----------+---------+---------------------------+---------------------------+-------------+----------+---------------------------------+-------------+
+
 
       $query = "SELECT * FROM mrbs_room WHERE room_name = '".$aula."'";
       $result = $mysqli->query($query);
@@ -124,10 +130,19 @@ function parseFile ($file, $bdhost, $bduser, $bdpass, $bdname, $pod_user_id) {
             // | ical_recur_id | varchar(16)         | NO   |     |                   |                             |
             // +---------------+---------------------+------+-----+-------------------+-----------------------------+
 
+            // +----+------------+------------+------------+-----------+---------+---------------------+-----------+------------------+-------------+------+-------------------------+-----------------+--------+----------+-----------+-----------+-----------+--------------------------------------------+---------------+------------------+
+            // | id | start_time | end_time   | entry_type | repeat_id | room_id | timestamp           | create_by | name             | profesor    | type | description             | Observaciones   | status | reminded | info_time | info_user | info_text | ical_uid                                   | ical_sequence | ical_recur_id    |
+            // +----+------------+------------+------------+-----------+---------+---------------------+-----------+------------------+-------------+------+-------------------------+-----------------+--------+----------+-----------+-----------+-----------+--------------------------------------------+---------------+------------------+
+            // | 41 | 1382434200 | 1382437800 |          1 |         1 |     123 | 2013-10-16 18:57:13 | rsierra   | test-borrar      | borrar      | B    | test-borrar             |                 |      0 |     NULL |      NULL | NULL      | NULL      | MRBS-525ec55fc1a72-69227c49@apoyotic.us.es |             0 | 20131022T093000Z |
+            // +----+------------+------------+------------+-----------+---------+---------------------+-----------+------------------+-------------+------+-------------------------+-----------------+--------+----------+-----------+-----------+-----------+--------------------------------------------+---------------+------------------+
+
+            //calculate timestamps
+            $tinicio = strtotime('2012-07-25 14:35:08' );;
+            $tfin = strtotime('2012-07-25 14:35:08' );;
 
             if($count == 0) {
               //Hacemos la reserva
-              $query = "INSERT INTO mrbs_entry (start_time, end_time, entry_type, repeat_id, room_id, create_by, name, profesor, type, ical_uid, ical_recur_id) VALUES ($finicio, $ffin, entry_type, 1, ".$room['id'].", '$pod_user_id', '$asig', '$prof', 'B', '20131017T093000Z', '00Z')";
+              $query = "INSERT INTO mrbs_entry (start_time, end_time, entry_type, repeat_id, room_id, create_by, name, profesor, type, ical_uid, ical_recur_id) VALUES ($tinicio, $tfin, 1, 1, ".$room['id'].", '$pod_user_id', '$asig', '$prof', 'B', '20131017T093000Z', '00Z')";
             } else {
               //Actualizamos la reserva
               $query = "UDATE mrbs_entry WHERE create_by = $pod_user_id AND room_name =".$aula;
