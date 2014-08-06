9409c6123fc44fc5048e998d8fbde888e953b9f8
Víctor Téllez <tellez.victor@gmail.com>
2014-08-06 22:59:59 +0200

    fixed

--- lib/parser.php
+++ lib/parser.php
@@ -55,7 +55,7 @@ function parseFile ($file, $bdhost, $bduser, $bdpass, $bdname, $pod_user_id) {
       $result = $mysqli->query($query);
 
       if ($result->num_rows != 1) {
-        $critical .= $actual_line.$line."\nMOTIVO: El aula '$aula' no existe en el sistema.\n\n";
+        $critical .= $actual_line.$line."\nMOTIVO: El aula '$aula' no existe en el sistema. $result->num_rows\n\n";
       }else {
           //Comprobamos que esté disponible el aula para esa fecha y horas
           $libre = true;
