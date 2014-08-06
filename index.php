4fac4274843b7895183fa07f220f244cc1e9e82b
Víctor Téllez <tellez.victor@gmail.com>
2014-08-06 17:19:31 +0200

    fixed

--- index.php
+++ index.php
@@ -134,17 +134,6 @@ LINEA N =>  ENF;35;350029;Evaluación de las Intervenciones de Enfermería en la
           <br/>
           <h3 style="color:#F89200;"><i class="fa fa-warning"></i>&nbsp; Reservas NO realizadas por ocupación:</h3>
           <textarea rows="10" style="width: 100%;">
-LINEA N =>  ENF;35;350008;Bioquímica;S;Q4;1;;00.00;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350001;Ciencias Psicosociales Aplicadas;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350028;Enfermería Clínica;S;C1;1;999;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350002;Enfermería Comunitaria I;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350015;Enfermería Comunitaria II;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350022;Enfermería Comunitaria III;S;A1;1;11;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350033;Enfermería en Situaciones de Urgencias y Emergencias Sanitarias;S;A1;1;999;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350024;Enfermería Geriátrica;S;A1;1;999;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350020;Enfermería Ginecológica y Planifinicación Familiar;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350013;Enfermería Materno-Infantil;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350014;Enfermería Médico-Quirúrgica I;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
 LINEA N =>  ENF;35;350023;Enfermería Médico-Quirúrgica II;S;A1;1;999;;;  ,;;;;SIN;:;:;SIN
 LINEA N =>  ENF;35;350021;Enfermería Pediátrica Social;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
 LINEA N =>  ENF;35;350025;Enfermería Psiquiátrica y de Salud Mental;S;C1;1;999;;;  ,;;;;SIN;:;:;SIN
@@ -159,23 +148,15 @@ LINEA N =>  ENF;35;350029;Evaluación de las Intervenciones de Enfermería en la
           <br/>
           <h3 style="color:#B24747;"><i class="fa fa-times"></i>&nbsp;Reservas NO realizadas por errores fatales:</h3>
           <textarea rows="10" style="width: 100%;">
-LINEA N =>  ENF;35;350008;Bioquímica;S;Q4;1;;00.00;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350001;Ciencias Psicosociales Aplicadas;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350028;Enfermería Clínica;S;C1;1;999;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350002;Enfermería Comunitaria I;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350015;Enfermería Comunitaria II;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350022;Enfermería Comunitaria III;S;A1;1;11;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350033;Enfermería en Situaciones de Urgencias y Emergencias Sanitarias;S;A1;1;999;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350024;Enfermería Geriátrica;S;A1;1;999;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350020;Enfermería Ginecológica y Planifinicación Familiar;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350013;Enfermería Materno-Infantil;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350014;Enfermería Médico-Quirúrgica I;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350023;Enfermería Médico-Quirúrgica II;S;A1;1;999;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350021;Enfermería Pediátrica Social;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350025;Enfermería Psiquiátrica y de Salud Mental;S;C1;1;999;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350004;Estructura y Función del Cuerpo Humano. Aspectos Funcionales;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350003;Estructura y Función del Cuerpo Humano. Aspectos Morfológicos;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN
-LINEA N =>  ENF;35;350029;Evaluación de las Intervenciones de Enfermería en la Comunidad y Programas de 
+LINEA N =>  ENF;35;350008;Bioquímica;S;Q4;1;;00.00;;  ,;;;;SIN;:;:;SIN 
+(MOTIVO: Formato de línea incorrecto)
+
+LINEA N =>  ENF;35;350001;Ciencias Psicosociales Aplicadas;S;Q4;1;;;;  ,;;;;SIN;:;:;SIN 
+(MOTIVO: Aula solicitada no existe en el sistema)
+
+LINEA N =>  ENF;35;350028;Enfermería Clínica;S;C1;1;999;;;  ,;;;;SIN;:;:;SIN  
+(MOTIVO: Formato de línea incorrecto)
+
           </textarea>
           
         </fieldset>
