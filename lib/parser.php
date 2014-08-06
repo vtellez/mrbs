0b9abc7812bb72cac76a10ea8339aeeb6ec1e0ac
Víctor Téllez <tellez.victor@gmail.com>
2014-08-06 23:55:26 +0200

    fixed

--- lib/parser.php
+++ lib/parser.php
@@ -50,12 +50,31 @@ function parseFile ($file, $bdhost, $bduser, $bdpass, $bdname, $pod_user_id) {
 
     if (!$errorline) {
       //Comprobamos que exista el aula
+
+      //MRBS ROOM
+      // +------------------+-------------+------+-----+---------+----------------+
+      // | Field            | Type        | Null | Key | Default | Extra          |
+      // +------------------+-------------+------+-----+---------+----------------+
+      // | id               | int(11)     | NO   | PRI | NULL    | auto_increment |
+      // | disabled         | tinyint(1)  | NO   |     | 0       |                |
+      // | area_id          | int(11)     | NO   |     | 0       |                |
+      // | room_name        | varchar(25) | NO   |     |         |                |
+      // | sort_key         | varchar(25) | NO   | MUL |         |                |
+      // | description      | varchar(60) | YES  |     | NULL    |                |
+      // | capacity         | int(11)     | NO   |     | 0       |                |
+      // | room_admin_email | text        | YES  |     | NULL    |                |
+      // | custom_html      | text        | YES  |     | NULL    |                |
+      // +------------------+-------------+------+-----+---------+----------------+
+
+
       $query = "SELECT * FROM mrbs_room WHERE room_name = '".$aula."'";
       $result = $mysqli->query($query);
 
       if ($result->num_rows < 1) {
         $critical .= $actual_line.$line." (MOTIVO: El aula '$aula' no existe en el sistema.)\n\n";
       }else {
+          $room = $result->fetch_assoc();
+
           //Comprobamos que esté disponible el aula para esa fecha y horas
           $libre = true;
 
@@ -78,10 +97,37 @@ function parseFile ($file, $bdhost, $bduser, $bdpass, $bdname, $pod_user_id) {
             //     echo $row['count'] . '<br />';
             // }
 
+            // MRBS ENTRY
+            // +---------------+---------------------+------+-----+-------------------+-----------------------------+
+            // | Field         | Type                | Null | Key | Default           | Extra                       |
+            // +---------------+---------------------+------+-----+-------------------+-----------------------------+
+            // | id            | int(11)             | NO   | PRI | NULL              | auto_increment              |
+            // | start_time    | int(11)             | NO   | MUL | 0                 |                             |
+            // | end_time      | int(11)             | NO   | MUL | 0                 |                             |
+            // | entry_type    | int(11)             | NO   |     | 0                 |                             |
+            // | repeat_id     | int(11)             | NO   |     | 0                 |                             |
+            // | room_id       | int(11)             | NO   |     | 1                 |                             |
+            // | timestamp     | timestamp           | NO   |     | CURRENT_TIMESTAMP | on update CURRENT_TIMESTAMP |
+            // | create_by     | varchar(80)         | NO   |     |                   |                             |
+            // | name          | varchar(90)         | NO   |     |                   |                             |
+            // | profesor      | varchar(70)         | NO   |     |                   |                             |
+            // | type          | char(1)             | NO   |     | E                 |                             |
+            // | description   | text                | YES  |     | NULL              |                             |
+            // | Observaciones | text                | YES  |     | NULL              |                             |
+            // | status        | tinyint(3) unsigned | NO   |     | 0                 |                             |
+            // | reminded      | int(11)             | YES  |     | NULL              |                             |
+            // | info_time     | int(11)             | YES  |     | NULL              |                             |
+            // | info_user     | varchar(80)         | YES  |     | NULL              |                             |
+            // | info_text     | text                | YES  |     | NULL              |                             |
+            // | ical_uid      | varchar(255)        | NO   |     |                   |                             |
+            // | ical_sequence | smallint(6)         | NO   |     | 0                 |                             |
+            // | ical_recur_id | varchar(16)         | NO   |     |                   |                             |
+            // +---------------+---------------------+------+-----+-------------------+-----------------------------+
+
 
             if($count == 0) {
               //Hacemos la reserva
-              $query = "INSERT INTO mrbs_entry (start_time, end_time, entry_type, repeat_id, room_id, timestamp, create_by, name, profesor, type, description, Observaciones, status, reminded, info_time, info_user, ical_uid, ical_sequence, ical_recur_id) VALUES ($finicio, $ffin, entry_type, 1, room_id, timestamp, $pod_user_id, name, profesor, type, description, Observaciones, status, reminded, info_time, info_user, ical_uid, ical_sequence, ical_recur_id)";
+              $query = "INSERT INTO mrbs_entry (start_time, end_time, entry_type, repeat_id, room_id, create_by, name, profesor, type, ical_uid, ical_recur_id) VALUES ($finicio, $ffin, entry_type, 1, $room['id'], '$pod_user_id', '$asig', '$prof', 'B', '20131017T093000Z', '00Z')";
             } else {
               //Actualizamos la reserva
               $query = "UDATE mrbs_entry WHERE create_by = $pod_user_id AND room_name =".$aula;
