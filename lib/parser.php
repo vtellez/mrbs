b146a96362a4e1c78f49a0b4165d581233eafb8a
Víctor Téllez <tellez.victor@gmail.com>
2014-08-07 11:00:05 +0200

    fixed

--- lib/parser.php
+++ lib/parser.php
@@ -154,6 +154,7 @@ function parseFile ($file, $bdhost, $bduser, $bdpass, $bdname, $pod_user_id) {
                                              end_time = $tfin
                                          WHERE room_id = ".$room['id']." AND start_time <= $tinicio AND end_time >= $tfin AND create_by = '$pod_user_id'";
             }
+          echo "<p>$query</p>";
 
             // $result = $mysqli->query($query); 
             $done .= $actual_line.$line."     Produce: $query\n";
