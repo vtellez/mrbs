c001bf496281c8fc6514a5aac6c476f4bbe01fb1
Víctor Téllez <tellez.victor@gmail.com>
2014-09-03 10:20:02 +0200

    fixed

--- lib/parser.php
+++ lib/parser.php
@@ -128,7 +128,7 @@ function parseFile ($file, $bdhost, $bduser, $bdpass, $bdname, $pod_user_id) {
 
     if (!$errorline) {
 
-
+      $done .= $actual_line.$line;
       foreach ($reps as $repdate) {
         //calculate timestamps
         $date = str_replace('/', '-', $repdate);
@@ -174,7 +174,7 @@ function parseFile ($file, $bdhost, $bduser, $bdpass, $bdname, $pod_user_id) {
               //                              WHERE room_id = ".$room['id']." AND start_time <= $tinicio AND end_time >= $tfin AND create_by = '$pod_user_id'";
               // }
               // $result = $mysqli->query($query); 
-              $done .= $actual_line.$line."     Produce: $query\n";
+              $done .= "<br/>     Produce: $query\n";
             }
         }
 
