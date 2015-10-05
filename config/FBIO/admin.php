42ce77ab7ef896a77534c236ce335b09b704cc8c
Victor Tellez <tellez.victor@gmail.com>
2015-10-05 15:25:01 +0200

    FIXED

---
+++ config/FBIO/admin.php
@@ -0,0 +1,22 @@
+<?php
+
+
+/**
+ * Database config template
+ *
+ *
+ * @file     admin.php
+ * @category Adminstradores por uvus
+ * @author   Víctor Téllez <tellez.victor@gmail.com>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ */
+
+
+$uvus_perm = array(
+
+					// 'rsierra', 
+					'vtellez-ext'
+					
+			);
+
+?>
