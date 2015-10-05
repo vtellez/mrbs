8b0ce64033abb070bd4eefbec4f1522d02d5e06e
Victor Tellez <tellez.victor@gmail.com>
2015-10-05 21:16:02 +0200

    fixed

--- lib/parser.php
+++ lib/parser.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Main parser
  * @file     config.php
@@ -8,16 +7,28 @@
  * @license  http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
  */
 
-
-function parseFile ($file, $bdhost, $bduser, $bdpass, $bdname, $pod_user_id) {
+function parseFile ($file, $centro) {
   //Comprobamos que existe el fichero
   if (!file_exists($file)) {
     return false;
   }
 
+  //Comprobamos que existe el centro
+  $centros = array('ci','eps','etsi','etsia','etsiinf','etsa','etsie','fba','fbio','fced','fcee','fcom','fct','fder','fefp','ffa','ffilol','ffilos','ffis','fgh','fmat','fmed','fodon','fpsi','fqui','ftf',);
+  if ( !in_array($centro, $centros) ) {
+    return false;
+  }
+
+  require_once 'config/'.$centro.'/admin.php';
+  require_once 'config/'.$centro.'/database.php';
+  //Comprobamos que el usuario administra el centro
+  if ( !in_array($uvus, $uvus_perm) ) {
+    return false;
+  }
+
   //Abrimos conexión con la base de datos
   $mysqli = new mysqli($bdhost, $bduser, $bdpass, $bdname);
- 
+
   if($mysqli->connect_errno) {
     return false;
   }
@@ -66,9 +77,9 @@ function parseFile ($file, $bdhost, $bduser, $bdpass, $bdname, $pod_user_id) {
 
       if(!isValidTimeStamp($tinicio) || !isValidTimeStamp($tfin) || $tinicio > $tfin) {
 
-          $critical .= $actual_line.$line." (MOTIVO: Formato de fechas incorrecto.)\n\n";
-          $errorline = true;
-      
+        $critical .= $actual_line.$line." (MOTIVO: Formato de fechas incorrecto.)\n\n";
+        $errorline = true;
+
       } else {
 
         $lunes = array();
@@ -80,57 +91,57 @@ function parseFile ($file, $bdhost, $bduser, $bdpass, $bdname, $pod_user_id) {
         $domingo = array();
 
         for ( $date = $tinicio; $date <= $tfin; $date += 60 * 60 * 24) {
-            if ( strftime('%w', $date) == 1 )  {
-                $lunes[] = strftime('%A %Y-%m-%d', $date);
-            } if ( strftime('%w', $date) == 2 ) {
-                $martes[] = strftime('%A %Y-%m-%d', $date);
-            } if ( strftime('%w', $date) == 3 ) {
-                $miercoles[] = strftime('%A %Y-%m-%d', $date);
-            } if ( strftime('%w', $date) == 4 ) {
-                $jueves[] = strftime('%A %Y-%m-%d', $date);
-            } if ( strftime('%w', $date) == 5 ) {
-                $viernes[] = strftime('%A %Y-%m-%d', $date);
-            } if ( strftime('%w', $date) == 6 ) {
-                $sabado[] = strftime('%A %Y-%m-%d', $date);
-            } if ( strftime('%w', $date) == 0 ) {
-                $domingo[] = strftime('%A %Y-%m-%d', $date);
-            }
+          if ( strftime('%w', $date) == 1 )  {
+            $lunes[] = strftime('%A %Y-%m-%d', $date);
+          } if ( strftime('%w', $date) == 2 ) {
+            $martes[] = strftime('%A %Y-%m-%d', $date);
+          } if ( strftime('%w', $date) == 3 ) {
+            $miercoles[] = strftime('%A %Y-%m-%d', $date);
+          } if ( strftime('%w', $date) == 4 ) {
+            $jueves[] = strftime('%A %Y-%m-%d', $date);
+          } if ( strftime('%w', $date) == 5 ) {
+            $viernes[] = strftime('%A %Y-%m-%d', $date);
+          } if ( strftime('%w', $date) == 6 ) {
+            $sabado[] = strftime('%A %Y-%m-%d', $date);
+          } if ( strftime('%w', $date) == 0 ) {
+            $domingo[] = strftime('%A %Y-%m-%d', $date);
+          }
         }
 
         $reps = array();
         switch ($dia) {
           case 'LUN':
-            $reps = $lunes;
-            break;
+          $reps = $lunes;
+          break;
           case 'MAR':
-            $reps = $martes;
-            break;
+          $reps = $martes;
+          break;
           case 'MIE':
-            $reps = $miercoles;
-            break;
+          $reps = $miercoles;
+          break;
           case 'JUE':
-            $reps = $jueves;
-            break;
+          $reps = $jueves;
+          break;
           case 'VIE':
-            $reps = $viernes;
-            break;
+          $reps = $viernes;
+          break;
           case 'SAB':
-            $reps = $sabado;
-            break;
+          $reps = $sabado;
+          break;
           case 'DOM':
-            $reps = $domingo;
-            break;
+          $reps = $domingo;
+          break;
           default:
-            $critical .= $actual_line.$line." (MOTIVO: Formato de fechas incorrecto.)\n\n";
-            $errorline = true;
-            break;
+          $critical .= $actual_line.$line." (MOTIVO: Formato de fechas incorrecto.)\n\n";
+          $errorline = true;
+          break;
         }
 
 
 
 
       }
-    
+
     }
 
     if (!$errorline) {
@@ -150,33 +161,33 @@ function parseFile ($file, $bdhost, $bduser, $bdpass, $bdname, $pod_user_id) {
         if ($result->num_rows < 1) {
           $critical .= $actual_line.$line." (MOTIVO: El aula '$aula' no existe en el sistema.)\n\n";
         }else {
-            $room = $result->fetch_assoc();
+          $room = $result->fetch_assoc();
 
             //Comprobamos que esté disponible el aula para esa fecha y horas
-            $libre = true;
+          $libre = true;
 
-            $query = "SELECT * FROM mrbs_entry WHERE room_id = ".$room['id']." AND start_time <= $tinicio AND end_time >= $tfin AND create_by <> '$pod_user_id'";
-            $result = $mysqli->query($query);
+          $query = "SELECT * FROM mrbs_entry WHERE room_id = ".$room['id']." AND start_time <= $tinicio AND end_time >= $tfin AND create_by <> '$pod_user_id'";
+          $result = $mysqli->query($query);
             // echo "<p>$query</p>";
 
-            if($result->num_rows > 0){
+          if($result->num_rows > 0){
 
-              $warnings .= $actual_line.$line."\n";
+            $warnings .= $actual_line.$line."\n";
             
-            } else {
-              
-              //Comprobamos si ya existía una reserva
-              $query = "SELECT * FROM mrbs_entry WHERE room_id = ".$room['id']." AND start_time <= $tinicio AND end_time >= $tfin AND create_by = '$pod_user_id'";
-              $result = $mysqli->query($query);
+          } else {
 
-              if($result->num_rows == 0) {
+              //Comprobamos si ya existía una reserva
+            $query = "SELECT * FROM mrbs_entry WHERE room_id = ".$room['id']." AND start_time <= $tinicio AND end_time >= $tfin AND create_by = '$pod_user_id'";
+            $result = $mysqli->query($query);
+
+            if($result->num_rows == 0) {
                 //Hacemos la reserva
-                $query = "INSERT INTO mrbs_entry (start_time, end_time, entry_type, repeat_id, room_id, create_by, name, profesor, type, ical_uid, ical_recur_id) VALUES ($tinicio, $tfin, 0, 0, ".$room['id'].", '$pod_user_id', '$asig', '$prof', 'B', '20131017T093000Z', '00Z')";
-              }
-             
-              $result = $mysqli->query($query); 
-              $done .= "\n".$actual_line.$line."  Produce: $query\n";
+              $query = "INSERT INTO mrbs_entry (start_time, end_time, entry_type, repeat_id, room_id, create_by, name, profesor, type, ical_uid, ical_recur_id) VALUES ($tinicio, $tfin, 0, 0, ".$room['id'].", '$pod_user_id', '$asig', '$prof', 'B', '20131017T093000Z', '00Z')";
             }
+
+            $result = $mysqli->query($query); 
+            $done .= "\n".$actual_line.$line;
+          }
         }
 
       }//foreach reps
@@ -200,7 +211,7 @@ function parseFile ($file, $bdhost, $bduser, $bdpass, $bdname, $pod_user_id) {
 
 function isValidTimeStamp($timestamp)
 {
-    if(strtotime(date('d-m-Y H:i:s',$timestamp)) === (int)$timestamp) {
-        return $timestamp;
-    } else return false;
+  if(strtotime(date('d-m-Y H:i:s',$timestamp)) === (int)$timestamp) {
+    return $timestamp;
+  } else return false;
 }
